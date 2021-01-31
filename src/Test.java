6a2c77afebdf132a90a7e376754b8391a520c11f
wcy <1476444846@qq.com>
2021-01-31 15:35:08 +0800

    解决用户名密码错误提示重新输入：

--- src/Test.java
+++ src/Test.java
@@ -6,32 +6,35 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
+        boolean bo=true;
+        while(bo) {
+            System.out.println("请输入用户名");
 
-        System.out.println("请输入用户名");
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();
+            System.out.println("刚刚输入的用户名:" + username);
 
-        Scanner sc = new Scanner(System.in);
-        String username = sc.next();
-        System.out.println("刚刚输入的用户名:" + username);
-
-        System.out.println("请输入密码");
-        String password = sc.next();
-        System.out.println("刚刚输入密码是:" + password);//用于检测输入的用户名是否正确
+            System.out.println("请输入密码");
+            String password = sc.next();
+            System.out.println("刚刚输入密码是:" + password);//用于检测输入的用户名是否正确
 
             /*
     开始读取文件
     */
-        //File file = new File("D:\\CmdShop\\src\\users.xlsx");
-        InputStream in = Class.forName("Test").getResourceAsStream("users.xlsx");//此方法能返回一个输入流，满足ReadExcel要求的输入流
-        ReadExcel readExcel = new ReadExcel();
-        User users[] = readExcel.readExcel(in);
+            //File file = new File("D:\\CmdShop\\src\\users.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("users.xlsx");//此方法能返回一个输入流，满足ReadExcel要求的输入流
+            ReadExcel readExcel = new ReadExcel();
+            User users[] = readExcel.readExcel(in);
 
-        System.out.println(users.length);
-        for (User user : users) {
-            if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
-                System.out.println("登录成功");
-                break;
-            } else {
-                System.out.println("登陆失败");
+            System.out.println(users.length);
+            for (User user : users) {
+                if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
+                    System.out.println("登录成功");
+                    bo=false;
+                    break;
+                } else {
+                    System.out.println("登陆失败");
+                }
             }
         }
     }
